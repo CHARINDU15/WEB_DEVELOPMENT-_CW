8baa7e1e623fdef6ae4df8cf626cc3d9b9975361
CHARINDU GAMAGE <charindu15@gmail.com>
2023-09-21 12:51:48 +0530

    change vedio bar
    change vedio frame

--- src/components/molecules/NavBar/index.jsx
+++ src/components/molecules/NavBar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { Button, Modal } from 'react-bootstrap';
 
 
 const NavBar = ({ darkMode, toggleTheme }) => {
@@ -10,16 +10,19 @@ const NavBar = ({ darkMode, toggleTheme }) => {
     setIsOpen(!isOpen);
   };
 
+  const [showModal, setShowModal] = useState(false);
 
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
   return (
     <>
-    
+
       <nav className={`navbar navbar-expand-lg navbar-dark bg-black border-bottom border-body fixed-top ${isOpen ? 'active' : ''}`}>
         <div className="container-fluid">
 
-        
-        
-      
+
+
+
           <a className="navbar-brand" href="#">
             <img src="./Untitled-1.jpg" alt="Logo" width="50" height="40" />
           </a>
@@ -57,7 +60,7 @@ const NavBar = ({ darkMode, toggleTheme }) => {
                 </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-disabled="false" herf = '/VENDORS'> VENDORS</a>
+                <a className="nav-link" aria-disabled="false" herf='/VENDORS'> VENDORS</a>
               </li>
             </ul>
             <form className="d-flex" role="search">
@@ -65,14 +68,118 @@ const NavBar = ({ darkMode, toggleTheme }) => {
               <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
 
-            <Link to="/login" href = '../'className="btn btn-outline-success">Login</Link>
+            <Link to="/login" className="btn btn-outline-success" onClick={handleShow}>
+              Login
+            </Link>
 
-             
-            </div>
+            <Modal show={showModal} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Login Modal</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                {/* Add your login form or content here */}
+                {/* For example: */}
+                <div className="wrapper">
+      <span className="close-icon">
+        <ion-icon name="close-outline"></ion-icon>
+      </span>
+
+      <div className="form-box login">
+        <h2>Login</h2>
+        <form action="#">
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="mail"></ion-icon>
+            </span>
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="lock-closed"></ion-icon>
+            </span>
+            <input type="Password" required />
+            <label>Password</label>
+          </div>
+          <div className="remember-forget">
+            <label>
+              <input type="checkbox" />Remember Me
+            </label>
+            <a href="#">Forgot Password ?</a>
+          </div>
+          <button type="submit" className="btn">
+            Login
+          </button>
+          <div className="login-register">
+            <p>
+              Don't have an account ?<a href="#" className="register-link">
+                Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+
+      <div className="form-box register">
+        <h2>Registration</h2>
+        <form action="#">
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="person"></ion-icon>
+            </span>
+            <input type="text" required />
+            <label>Username</label>
+          </div>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="mail"></ion-icon>
+            </span>
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="lock-closed"></ion-icon>
+            </span>
+            <input type="Password" required />
+            <label>Password</label>
+          </div>
+          <div className="remember-forget">
+            <label>
+              <input type="checkbox" />I agree to the terms & conditions
+            </label>
+          </div>
+          <button type="submit" className="btn">
+            Register
+          </button>
+          <div className="login-register">
+            <p>
+              Already have an account ?
+              <a href="#" className="login-link">
+                Login
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Login
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+
+          </div>
 
         </div>
 
-        
+
       </nav>
     </>
   );
